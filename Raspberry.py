3004741b0bc4cb67bfa84bd7cdacbbfc55a16d8e
AdrianAdam <30503947+AdrianAdam@users.noreply.github.com>
2020-02-29 15:03:49 +0200

    Added AES 256 encryption, messages with progress, made small changes

--- Raspberry.py
+++ Raspberry.py
@@ -9,6 +9,7 @@ import subprocess
 import datetime
 from picamera import PiCamera
 from google.cloud import storage
+from Crypto.Cipher import AES
 from apscheduler.schedulers.background import BackgroundScheduler
 
 ser=serial.Serial("/dev/ttyACM0",9600)
@@ -25,8 +26,11 @@ camera = PiCamera()
 camera.rotation = 180
 camera.resolution = (1280, 768)
 
+# To achieve AES 256, the key must have a length of 32 bits
+aes = AES.new("This needs to be a 32 bit key!!!", AES.MODE_CBC, "This is a 16 bit")
 
 def deleteVideos():
+    print("Started deleting older videos")
     global indexVideoDeleted
     if not indexVideo == indexVideoDeleted:
         for i in range(indexVideoDeleted, indexVideo - 1):
@@ -34,6 +38,7 @@ def deleteVideos():
             os.remove("/home/pi/Documents/Project/video{:03d}.mp4".format(i))
            
         indexVideoDeleted = indexVideo - 1
+    print("Finished deleting older videos")
         
         
 scheduler = BackgroundScheduler()
@@ -45,33 +50,45 @@ firebase = firebase.FirebaseApplication('', None) #link to your Firebase project
 client = storage.Client.from_service_account_json('') #path to your Firebase storage json file
 bucket = client.get_bucket('') #link to your storage bucket
 
-#subprocess.call("raspivid -t 0 -o - | nc 192.168.43.216 8090", shell=True) #not working attempt of live streaming
-
 try:
+    print("Started main loop")
+    print("----------------------------")
     while True:
         if ser.readline() is not None:
+            print("Received message")
             Input = ser.readline()
             time = datetime.datetime.now()
-            dataOut = {"message": "Intruder detected!", "datetime": time}
+            time = time.strftime("%Y-%M-%dT%I%M%S")
+            if len(time) < 32:
+                time += "!" * (32 - len(time))
+            message = "Sensor triggered"
+            encryptedMessage = aes.encrypt(message)
+            encryptedTime = aes.encrypt(time)
+            
+            dataOut = {"message": str(encryptedMessage)[2:-1], "datetime": str(encryptedTime)[2:-1]}
             firebase.post('/Door1/DT', dataOut)
 
+            print("Sent message")
+            print("Started camera preview")
+            
             camera.start_preview()
             camera.start_recording('/home/pi/Documents/Project/video{:03d}.h264'.format(indexVideo))
             sleep(10)
             camera.stop_recording()
             camera.stop_preview()
-    
+            
+            print("Finished camera preview")
+            print("Started file upload")
+            
             command = "MP4Box -add video{:03d}.h264 video{:03d}.mp4".format(indexVideo, indexVideo)
             output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
 
             videoBlob = bucket.blob(str(time))
             videoBlob.upload_from_filename(videoPath + "/video{:03d}.mp4".format(indexVideo))
+            
+            print("Finished file upload")
+            print("----------------------------")
     
             indexVideo += 1
 except KeyboardInterrupt:
     pass
-
-if not indexVideo == 0:
-    for i in range(indexVideosDeleted, indexVideo + 1):
-        os.remove("/home/pi/Documents/Project/video{:03d}.h264".format(i))
-        os.remove("/home/pi/Documents/Project/video{:03d}.mp4".format(i))
